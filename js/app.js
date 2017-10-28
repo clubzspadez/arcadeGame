b336d5f1b1bfedad4afcd2bb8c3b810eefa8c337
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-10-28 08:42:14 -0400

    updated movement speed and amount of bugs per lane

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ var Enemy = function(x, y, speed) {
     this.y = y;
     this.speed = speed;
     this.moving = false;
+    this.originX = x;
 };
 
 // Update the enemy's position, required method for game
@@ -20,12 +21,21 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+     this.x += this.speed * dt;
+     if(this.x > 500){
+        this.reset();
+        }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Enemy.prototype.reset= function(){
+        this.x = this.originX;
+}
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -37,41 +47,57 @@ var Player = function() {
     this.y = 400;
     this.moving = false;
 
-}
-Player.prototype.update = function(){
+    //update player
+    this.update = function(){
 
-}
-
-Player.prototype.render = function(){
-   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-Player.prototype.handleInput = function(key){
-    if(key === 'up' && this.y > 5){
-        this.y -= 90
-    }else if(key === 'down' && this.y < 400 ){
-        this.y += 90
-    }else if(key === 'left' && this.x > 0){
-        this.x -= 100
-    } else if(key === 'right' && this.x < 400){
-        this.x += 100
+    }
+    //render player
+    this.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    //handle input
+    this.handleInput = function(key){
+        if(key === 'up' && this.y > 5){
+            this.y -= 90
+        }else if(key === 'down' && this.y < 400 ){
+            this.y += 90
+        }else if(key === 'left' && this.x > 0){
+            this.x -= 100
+        } else if(key === 'right' && this.x < 400){
+            this.x += 100
+        }
+    }
+    //reset method
+    this.reset = function(){
+    this.x = 200;
+    this.y = 400;
+    this.moving = true;
     }
 }
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const player = new Player();
 //first row enemy
-const enemyOne = new Enemy(0, 220, 40);
-const enemyTwo = new Enemy(0, 130, 40);
-const enemyThree = new Enemy(0, 50, 40);
+const enemyOne = new Enemy(-1000, 220, 500);
+const enemyOneSec = new Enemy(-2000, 220, 500);
+const enemyTwo = new Enemy(-500, 130, 700);
+const enemyTwoSec = new Enemy(-1500, 130, 700);
+const enemyThree = new Enemy(-400, 50, 600);
+const enemyThreeSec = new Enemy(-1400, 50,  600);
 
 
 allEnemies.push(enemyOne);
+allEnemies.push(enemyOneSec);
 allEnemies.push(enemyTwo);
+allEnemies.push(enemyTwoSec);
 allEnemies.push(enemyThree);
+allEnemies.push(enemyThreeSec);
 
-
+//provided by mdn https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
